c9524282132dda4a20bd128286095c666285433a
cmd01 <cmd01@ya.ru>
2016-06-05 11:22:18 +0300

    создание класса для расчета площади квадрата

---
+++ test4.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+class Square:
+	def __init__("self", width, length):
+		self.width = width
+		self.length = length
+	def area:
+		return self.width*self.length
+
+square1=Square(10, 10)
