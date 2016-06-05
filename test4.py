da77fcb44b8386c65cddab4edb36a784517eb88c
cmd01 <cmd01@ya.ru>
2016-06-05 12:45:58 +0300

    test

--- test4.py
+++ test4.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 class Square:
-	def __init__("self", width, length):
+    def __init__(self, width, length):
 		self.width = width
-		self.length = length
-	def area:
-		return self.width*self.length
+        self.length = length
 
-square1=Square(10, 10)
-square2=Square(12, 12)
-square3=Square(100, 100)
+    def area(self):
+        return self.width * self.length
+
+square1 = Square(10, 10)
+square2 = Square(12, 12)
+square3 = Square(100, 100)
