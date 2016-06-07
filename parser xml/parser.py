8f0f928f96b10826b6b29e9319b3dfc15077a0c5
cmd01 <cmd01@ya.ru>
2016-06-07 11:18:59 +0300

    учимся использовать библиотеку ElementTree

--- parser xml/parser.py
+++ parser xml/parser.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 import os
-import xml.etree.cElementTree as ET
+import xml.etree.ElementTree as etree
 
 XML_FILE = os.path.join(os.environ['HOME'], 'xmlfile.xml')
 
-try:
-	tree = ET.ElementTree(file=XML_FILE)
-	print help(tree)
-except IOError as e:
-	print ('nERROR - cant find filr: %sn' % e)
+tree = etree.parse('/home/cmd01/xmlfile.xml')
+root = tree.getroot()
+print(root)
