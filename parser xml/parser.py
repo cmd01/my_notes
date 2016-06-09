570747a054f036d7c005b456a95d4f4200f637e4
cmd01 <cmd01@ya.ru>
2016-06-09 10:16:08 +0300

    test

--- parser xml/parser.py
+++ parser xml/parser.py
@@ -4,6 +4,14 @@ import xml.etree.ElementTree as etree
 
 XML_FILE = os.path.join(os.environ['HOME'], 'xmlfile.xml')
 
-tree = etree.parse('/home/cmd01/xmlfile.xml')
+tree = tree.parse('/home/cmd01/xmlfile.xml')
 root = tree.getroot()
 print(root)
+print(root.tag)
+for child in root:
+		print(child)
+print(root.attrib)
+print(root[4])
+print(root[4].attrib)
+p = tree.find('AUD')
+print (p)
